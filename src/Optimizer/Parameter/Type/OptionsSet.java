683d38905f2184f0a13ef25489bbec2be10e0f25
Emad Alharbi <emra500@york.ac.uk>
2020-12-24 16:26:19 +0000

    add set of options parameter

---
+++ src/Optimizer/Parameter/Type/OptionsSet.java
@@ -0,0 +1,28 @@
+package Optimizer.Parameter.Type;
+
+import java.util.Vector;
+
+public class OptionsSet {
+
+	Vector<ItemGroup> options = new Vector<ItemGroup>(); 
+	
+	public OptionsSet(Vector<ItemGroup> options) {
+		this.options=options;
+	}
+public OptionsSet(OptionsSet os) {
+		this.options= new Vector<ItemGroup>(os.options);
+}
+	public OptionsSet() {
+		
+	}
+	public void add(ItemGroup i) {
+		options.add(i);
+	}
+	public int size() {
+		return options.size();
+	}
+	public ItemGroup GetByIndex(int i) {
+		return options.get(i);
+	}
+	
+}
